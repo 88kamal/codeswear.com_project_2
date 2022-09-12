8e233a92db93084bbb761cf18bbdca08fc16b8f2
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2022-09-12 23:48:36 +0530

    okk

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,71 @@
+import { useEffect, useState } from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  const [cart, setCart] = useState({})
+  const [subTotal, setSubTotal] = useState(0);
+  useEffect(() => {
+    // console.log('useef') 
+    try {
+      if (localStorage.getItem("cart")) {
+        setCart(JSON.parse(localStorage.getItem("cart")))
+        saveCart(JSON.parse(localStorage.getItem("cart")))
+      }
+    } catch (error) {
+      console.log(error)
+      localStorage.clear()
+    }
+    // saveCart(cart)
 
+  }, [])
+
+
+  const saveCart = (myCart) => {
+    localStorage.setItem("cart", JSON.stringify(myCart))
+    let subt = 0;
+    let keys = Object.keys(myCart)
+    for (let i = 0; i < keys.length; i++) {
+      // console.log(keys)
+      subt += myCart[keys[i]].price * myCart[keys[i]].qty
+    }
+    setSubTotal(subt)
+  }
+  const addToCart = (itemCode, qty, price, name, size, varient) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty + qty
+    }
+    else (
+      newCart[itemCode] = { qty: 1, price, name, size, varient }
+
+    )
+    setCart(newCart)
+    // console.log(newCart)
+    saveCart(newCart)
+  }
+  const clearCart = () => {
+    setCart({})
+    saveCart({})
+  }
+  const removeFromCart = (itemCode, qty, price, name, size, varient) => {
+    let newCart = JSON.parse(JSON.stringify(cart));
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty - qty
+    }
+    if (newCart[itemCode]["qty"] <= 0) {
+      delete newCart[itemCode]
+    }
+    setCart(newCart)
+    saveCart(newCart)
+  }
+  return (
+    <>
+      <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} />
+      <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} {...pageProps} />
+      <Footer />
+    </>
+  )
+}
 export default MyApp
---
+++ pages/tshirts.js
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+import React from 'react'
+import Product from '../models/Product'
+import mongoose from 'mongoose'
+
+function Tshirts({ products }) {
+
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-12 mx-auto">
+          <div className="flex flex-wrap -m-4 justify-center">
+            {
+              products.map((item) => {
+                return (
+
+                  <Link passHref={true} href={`/product/${item.slug}`} key={item._id}>
+                    <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-6">
+                      <a style={{
+                        position: 'inherit'
+                      }} className="block relative  rounded overflow-hidden">
+                        <img alt="ecommerce" className=" h-[40vh] md:h-[36vh] block m-auto" src={item.img} />
+                      </a>
+                      <div className="mt-4 text-center md:text-left">
+                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                        <h2 className="text-gray-900 title-font text-lg font-medium">{item.title}</h2>
+                        <p className="mt-1">₹ {item.price}</p>
+                        <p className="mt-1">S, M, L, XL, XXL</p>
+                      </div>
+                    </div>
+                  </Link>
+
+                )
+              })
+            }
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+
+  }
+  await mongoose.connect(process.env.MONGO_URI)
+  let products = await Product.find({category:'tshirt'})
+
+
+  return {
+    props: { products: JSON.parse(JSON.stringify((products))) }, // will be passed to the page component as props
+  }
+}
+
+export default Tshirts
